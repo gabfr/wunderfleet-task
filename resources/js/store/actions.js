f23e31c9017a691fa54be1541471b3dd87b3e71e
Gabriel <eu@gabrielf.com>
2019-08-14 20:05:51 -0300

    feature: creating the customer and then fetching it until paymentDataId is available (with interval of 5 secs between each request)

--- resources/js/store/actions.js
+++ resources/js/store/actions.js
@@ -2,13 +2,39 @@ import Vue from 'vue'
 import axios from 'axios';
 
 export default {
-    async persistCustomer ({commit, state}) {
-        try {
-            const response = await axios.post(`customers`, state.register_form);
-            const customer = response.data;
+    /**
+     * Fetch the persisted customer.
+     * If the remotePaymentDataId is not present, it will fetch again in 5 seconds.
+     * @param commit
+     * @param state
+     * @returns {Promise<void>}
+     */
+    async fetchPersistedCustomer ({commit, state, dispatch}) {
+        const persistedCustomer = state.persisted_customer;
+        if (persistedCustomer && persistedCustomer.id) {
+            const response = await axios.get(`api/customers/${persistedCustomer.id}`);
+            const customer = response.data.data;
             commit('setPersistedCustomer', customer);
-        } catch (err) {
-
+            if (!customer.remotePaymentDataId) {
+                window.setTimeout(() => dispatch('fetchPersistedCustomer'), 5000)
+            }
         }
+    },
+    /**
+     * Persist the customer.
+     * If the remotePaymentDataId is not available yet, we schedule a fetchPersistedCustomer again to fetch it
+     * @param commit
+     * @param state
+     * @param dispatch
+     * @returns {Promise<void>}
+     */
+    async persistCustomer ({commit, state, dispatch}) {
+        const response = await axios.post(`api/customers`, state.register_form);
+        const customer = response.data.data;
+        commit('setPersistedCustomer', customer);
+        if (!customer.remotePaymentDataId) {
+            window.setTimeout(() => dispatch('fetchPersistedCustomer'), 5000)
+        }
+        commit('gotoNextStep');
     }
 }
