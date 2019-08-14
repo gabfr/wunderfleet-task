62609df26c8cb3a994d44b1b9e0610dc87446906
Gabriel <eu@gabrielf.com>
2019-08-14 20:30:22 -0300

    feature: making the tests work without calling the API

--- tests/Feature/CustomersRegistrationTest.php
+++ tests/Feature/CustomersRegistrationTest.php
@@ -3,23 +3,58 @@
 namespace Tests\Feature;
 
 use App\Customer;
+use App\Events\CustomerWasCreated;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CustomersRegistrationTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic feature test example.
+     * Test the basic case of creation only.
      *
      * @return void
      */
     public function testCustomerRegistrationWithValidData()
     {
+        Event::fake();
         $customer = factory(Customer::class)->make();
 
         $response = $this->postJson('/api/customers', $customer->toArray());
 
         $response->assertStatus(201);
+        $responseBody = $response->getContent();
+        $responseJson = json_decode($responseBody, true);
+
+        $this->assertArrayHasKey('data', $responseJson);
+        $this->assertArrayNotHasKey('paymentDataId', $responseJson['data']);
+        $this->assertArrayHasKey('id', $responseJson['data']);
+        Event::assertDispatched(CustomerWasCreated::class);
+    }
+
+    /**
+     * Test the creation and check if the paymentDataId will be populated after api call
+     */
+    public function testCustomerShowRequest()
+    {
+        $customer = Event::fakeFor(function () {
+            $customer = factory(Customer::class)->create();
+
+            Event::assertDispatched(CustomerWasCreated::class);
+
+            return $customer;
+        });
+
+        $response = $this->getJson("/api/customers/{$customer->id}");
+        $response->assertStatus(200);
+        $responseBody = $response->getContent();
+        $responseJson = json_decode($responseBody, true);
+
+        $this->assertArrayHasKey('data', $responseJson);
+        $this->assertArrayNotHasKey('paymentDataId', $responseJson['data']);
+        $this->assertArrayHasKey('id', $responseJson['data']);
     }
 }
